835d956285209acf853eb3e8ad228ac6d3991eee
Michael Frederiksen <90389752+MagicalGlove@users.noreply.github.com>
2025-03-18 13:20:20 +0100

    Updates image selector in company edit page (#184)
    * Updates image selector in company edit page
    
    * Update details-form.tsx
    
    ---------
    
    Co-authored-by: Mads Knudsen <madsfknudsen@hotmail.com>

--- apps/client/src/pages/dashboard/company/_dialogs/details-form.tsx
+++ apps/client/src/pages/dashboard/company/_dialogs/details-form.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable lingui/no-unlocalized-strings */
 import { zodResolver } from "@hookform/resolvers/zod";
 import { t } from "@lingui/macro";
-import { UploadSimple } from "@phosphor-icons/react";
+import { Aperture, Trash, UploadSimple } from "@phosphor-icons/react";
 import type { CompanyDto, companySchema, UpdateCompanyDto } from "@reactive-resume/dto";
 import { updateCompanySchema } from "@reactive-resume/dto";
 import {
+  Avatar,
+  AvatarImage,
   Button,
   buttonVariants,
   Form,
@@ -13,15 +16,20 @@ import {
   FormLabel,
   FormMessage,
   Input,
+  Label,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
   RichInput,
 } from "@reactive-resume/ui";
 import { cn } from "@reactive-resume/utils";
 import { AnimatePresence, motion } from "framer-motion";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
-import type { z } from "zod";
+import { z } from "zod";
 
 import { AiActions } from "@/client/components/ai-actions";
+import { PictureOptions } from "@/client/pages/builder/sidebars/left/sections/picture/options";
 import { CompanyLogo } from "@/client/pages/dashboard/company/company-logo";
 import { useUpdateCompany } from "@/client/services/company";
 import { useUploadImage } from "@/client/services/storage";
@@ -77,17 +85,35 @@ export const DetailsForm = ({ companyState, setCompanyState }: Props) => {
     }
   };
 
-  const onSelectImage = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files.length > 0) {
-      const file = event.target.files[0];
-      const response = await uploadImage(file);
-      const url = response.data;
-
+  const onAvatarClick = async () => {
+    if (isValidUrl) {
       await updateCompany({
         id: companyState.id,
-        picture: url,
+        picture: null,
       });
+      form.setValue("picture", "");
+    } else {
+      inputRef.current?.click();
     }
+  };
+
+  const pictureUrl = form.watch("picture");
+  const isValidUrl = useMemo(() => z.string().url().safeParse(pictureUrl).success, [pictureUrl]);
+
+  const onSelectImage = async (event?: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event?.target.files?.length) return;
+
+    const file = event.target.files[0];
+    const response = await uploadImage(file);
+    const url = response.data;
+
+    await updateCompany({
+      id: companyState.id,
+      picture: url,
+    });
+
+    // Update form state
+    form.setValue("picture", url);
   };
 
   return (
@@ -96,17 +122,28 @@ export const DetailsForm = ({ companyState, setCompanyState }: Props) => {
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)}>
             <div style={{ marginBottom: "10px" }}>
-              <div style={{ marginBottom: "10px" }}>
-                <FormField
-                  name="picture"
-                  control={form.control}
-                  render={({ field, fieldState: { error } }) => (
-                    <div
-                      className={cn(
-                        "flex items-end gap-x-4 sm:col-span-2",
-                        error && "items-center",
+              <FormField
+                name="picture"
+                control={form.control}
+                render={({ field, fieldState: { error } }) => (
+                  <div
+                    className={cn("flex items-end gap-x-4 sm:col-span-2", error && "items-center")}
+                  >
+                    {" "}
+                    <div className="group relative cursor-pointer" onClick={onAvatarClick}>
+                      <Avatar className="size-14 bg-secondary">
+                        <AvatarImage src={pictureUrl ?? undefined} />
+                      </Avatar>
+
+                      {isValidUrl ? (
+                        <div className="pointer-events-none absolute inset-0 flex items-center justify-center rounded-full bg-background/30 opacity-0 transition-opacity group-hover:opacity-100">
+                          <Trash size={16} weight="bold" />
+                        </div>
+                      ) : (
+                        <div className="pointer-events-none absolute inset-0 flex items-center justify-center rounded-full bg-background/30 opacity-0 transition-opacity group-hover:opacity-100">
+                          <UploadSimple size={16} weight="bold" />
+                        </div>
                       )}
-                    >
                       <CompanyLogo company={companyState} />
                       <FormItem className="flex-1">
                         <FormLabel>Logo</FormLabel>
@@ -129,9 +166,37 @@ export const DetailsForm = ({ companyState, setCompanyState }: Props) => {
                         <UploadSimple />
                       </motion.button>
                     </div>
-                  )}
-                />
-              </div>
+                    <div className="flex w-full flex-col gap-y-1.5">
+                      <Label htmlFor="picture.url">{t`Picture`}</Label>
+                      <div className="flex items-center gap-x-2">
+                        <input ref={inputRef} hidden type="file" onChange={onSelectImage} />
+
+                        <Input placeholder="https://..." {...field} value={field.value ?? ""} />
+
+                        {isValidUrl && (
+                          <Popover>
+                            <PopoverTrigger asChild>
+                              <motion.button
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                disabled={isUploading}
+                                exit={{ opacity: 0 }}
+                                className={cn(buttonVariants({ size: "icon", variant: "ghost" }))}
+                              >
+                                <Aperture />
+                              </motion.button>
+                            </PopoverTrigger>
+                            <PopoverContent className="w-[360px]">
+                              <PictureOptions />
+                            </PopoverContent>
+                          </Popover>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                )}
+              />
+
               <FormField
                 name="name"
                 control={form.control}
@@ -189,6 +254,8 @@ export const DetailsForm = ({ companyState, setCompanyState }: Props) => {
                 </FormItem>
               )}
             />
+            {error && <div className="text-red-500">{error}</div>}
+
             <AnimatePresence presenceAffectsLayout>
               {form.formState.isDirty && (
                 <motion.div
